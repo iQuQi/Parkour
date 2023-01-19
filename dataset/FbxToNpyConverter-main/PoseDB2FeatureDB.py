Feature/modify-normalization, Fix feature value
Former-commit-id: 69fa2ee10aa2bfa11ae9ee54c06dd8d319753c79

dataset/FbxToNpyConverter-main/PoseDB2FeatureDB.py
@@ -8,6 +8,8 @@ import json
 from mathutils import Vector
 import numpy as np 
 
+import scipy.stats as ss 
+
 HOME_FILE_PATH = os.path.abspath('homefile.blend')
 MIN_NR_FRAMES = 64
 RESOLUTION = (512, 512)
@@ -73,10 +75,48 @@ def poseDB2featureDB():
     now_start_index = 0
     now_end_index = 0
 
+    # 표준화
+    HIP_KEY = 'mixamorig2:Hips' 
+    frames_np = np.array(frames)
+    list_hips = {'location': [], 'velocity':[]}
+    list_feet = {'Rfoot':{'tailLocation':[], 'velocity':[]}, 'Lfoot':{'tailLocation':[], 'velocity':[]}}
+    for i in range(len(frames_np)):
+        hip_frames = frames_np[i]['joints'][HIP_KEY]
+        list_hips['location'].append(np.array([hip_frames['location'][0]/100, hip_frames['location'][1]/100, hip_frames['location'][2]/100]))
+        list_hips['velocity'].append(frames_np[i]['joints'][HIP_KEY]['velocity'])
+        
+        list_feet['Rfoot']['tailLocation'].append(frames_np[i]['joints']['mixamorig2:RightFoot']['tailLocation'])
+        list_feet['Rfoot']['velocity'].append(frames_np[i]['joints']['mixamorig2:RightFoot']['velocity'])
+        list_feet['Lfoot']['tailLocation'].append(frames_np[i]['joints']['mixamorig2:LeftFoot']['tailLocation'])
+        list_feet['Lfoot']['velocity'].append(frames_np[i]['joints']['mixamorig2:LeftFoot']['velocity'])
+        
+    list_hips['location'] = ss.zscore(list_hips['location']).tolist()
+    list_hips['velocity'] = ss.zscore(list_hips['velocity']).tolist()
+    list_feet['Rfoot']['tailLocation'] = ss.zscore(list_feet['Rfoot']['tailLocation']).tolist()
+    list_feet['Rfoot']['velocity'] = ss.zscore(list_feet['Rfoot']['velocity']).tolist()
+    list_feet['Lfoot']['tailLocation'] = ss.zscore(list_feet['Lfoot']['tailLocation']).tolist()
+    list_feet['Lfoot']['velocity'] = ss.zscore(list_feet['Lfoot']['velocity']).tolist()
+
+    mean={'hips':{'location':np.mean(list_hips['location']), 'velocity':np.mean(list_hips['velocity'])}, 
+        'Rfoot':{'tailLocation': np.mean(list_feet['Rfoot']['tailLocation']), 'velocity':np.mean(list_feet['Rfoot']['velocity'])},
+        'Lfoot':{'tailLocation': np.mean(list_feet['Lfoot']['tailLocation']), 'velocity':np.mean(list_feet['Lfoot']['velocity'])},
+    }
+    std={'hips':{'location':np.std(list_hips['location']), 'velocity':np.std(list_hips['velocity'])}, 
+        'Rfoot':{'tailLocation': np.std(list_feet['Rfoot']['tailLocation']), 'velocity':np.std(list_feet['Rfoot']['velocity'])},
+        'Lfoot':{'tailLocation': np.std(list_feet['Lfoot']['tailLocation']), 'velocity':np.std(list_feet['Lfoot']['velocity'])},
+    }
+
+    standard_hips = []
+    standard_feet = []
+    for i in range(len(frames_np)):
+        standard_hips.append({'location': list_hips['location'][i], 'velocity':list_hips['velocity'][i]})
+        standard_feet.append({'Rfoot':{'tailLocation':list_feet['Rfoot']['tailLocation'][i], 'velocity':list_feet['Rfoot']['velocity'][i]}, 
+                                'Lfoot':{'tailLocation':list_feet['Lfoot']['tailLocation'][i], 'velocity':list_feet['Lfoot']['velocity'][i]}})
+
     for i in range(len(frames)):
         FRAME = frames[i]['joints']
         ANIM_INFO = frames[i]['animInfo'][0]
-        HIP_KEY = 'mixamorig2:Hips' 
+        
         print('anim info',ANIM_INFO)
 
         if i == 0:
@@ -92,23 +132,23 @@ def poseDB2featureDB():
              continue
 
         # 포즈 특징 채워주기
-        root_velocity = FRAME[HIP_KEY]['velocity'] 
+        root_velocity = standard_hips[i]['velocity'] 
 
-        Rfoot_location = FRAME['mixamorig2:RightFoot']['tailLocation']
-        Rfoot_velocity = FRAME['mixamorig2:RightFoot']['velocity']
+        Rfoot_location = standard_feet[i]['Rfoot']['tailLocation']
+        Rfoot_velocity = standard_feet[i]['Rfoot']['velocity']
 
-        Lfoot_location = FRAME['mixamorig2:LeftFoot']['tailLocation']
-        Lfoot_velocity = FRAME['mixamorig2:LeftFoot']['velocity']
+        Lfoot_location = standard_feet[i]['Lfoot']['tailLocation']
+        Lfoot_velocity = standard_feet[i]['Lfoot']['velocity']
 
 
         # 궤적 특징 채워주기
        
-        NOW = frames[i]['joints'][HIP_KEY]
-        FUTURE4 = frames[i+4]['joints'][HIP_KEY]
-        FUTURE8 = frames[i+8]['joints'][HIP_KEY]
-        FUTURE12 = frames[i+12]['joints'][HIP_KEY]
-        FUTURE16 = frames[i+16]['joints'][HIP_KEY]
-        FUTURE20 = frames[i+20]['joints'][HIP_KEY]
+        NOW = standard_hips[i]
+        FUTURE4 = standard_hips[i+4]
+        FUTURE8 = standard_hips[i+8]
+        FUTURE12 = standard_hips[i+12]
+        FUTURE16 = standard_hips[i+16]
+        FUTURE20 = standard_hips[i+20]
 
 
         # z_w = np.array([frames[i]['zAxis'][0], (-1)*frames[i]['zAxis'][2], frames[i]['zAxis'][1]]) # z_w
@@ -130,14 +170,14 @@ def poseDB2featureDB():
                                 global2local(FUTURE8['velocity'], M), global2local(FUTURE12['velocity'],M), 
                                 global2local(FUTURE16['velocity'], M), global2local(FUTURE20['velocity'], M)]
 
-        new_feature = Feature(global2local(root_velocity,M), global2local(Rfoot_location,M), Rfoot_velocity, global2local(Lfoot_location,M), 
+        new_feature = Feature(global2local(root_velocity,M), Rfoot_location, Rfoot_velocity, Lfoot_location, 
                         Lfoot_velocity, trajectory_location, trajectory_direction, ANIM_INFO['index'])
 
         # 각 파일에 들어갈 피쳐 배열에 추가
         features_npy.append(np.array(new_feature.__dict__))
         features_json.append(new_feature.__dict__)
         print('featureIndex', len(features_json)-1)
-
+    features_json.append({'mean':mean,'std':std})
     # npy, json 파일로 저장하기
     np.save(COMBINED_FILE_PATH + 'featureDB.npy', features_npy)
     save_path = os.path.join(COMBINED_FILE_PATH,'featureDB.json')

dataset/FbxToNpyConverter-main/fbx2PoseDB.py
@@ -20,6 +20,11 @@ X = 0
 Y = 1
 Z = 2
 
+def global2local(fir,M ):
+    tmp = fir.copy()
+    tmp.append(1)
+    return (M@tmp)[:-1].tolist()
+
 class AnimInfo:
     def __init__(self, name, index, start, end):
         self.name = name
@@ -140,8 +145,19 @@ def fbx2PoseDB():
                 if name[-4:]=='Hips':
                     out_dict['axes'] = np.array([bone_struct[name].x_axis, bone_struct[name].y_axis, bone_struct[name].z_axis]).tolist()
                     
+                # M matrix 생성
+                x_u = out_dict['axes'][0]
+                y_v = out_dict['axes'][1]
+                z_w = out_dict['axes'][2] # z_w
+                M = [[x_u[0], y_v[0], z_w[0], location[0]],
+                    [x_u[1], y_v[1], z_w[1],  location[1]],
+                    [x_u[2],  y_v[2], z_w[2], location[2]],
+                    [0, 0, 0, 1]]
+
+                M = np.linalg.inv(M)
+
                 # 새로운 joint 추가
-                joint = Joint(location, rotation, angular_velocity, velocity, tail_location)
+                joint = Joint(location, rotation, angular_velocity, velocity, global2local(tail_location,M))
                 out_dict['joints'][correct_name] = joint.__dict__
                 
                 prev_location[name] = location # name, correct_name 관련 없지만 일단 name으로 둠

scripts/PlayerInputController.py
@@ -88,6 +88,12 @@ class ModalOperator(bpy.types.Operator):
             elif value == 'RELEASE':
                 self.KEY_MAP[type] = False
 
+    def calculateStandard(self, data, mean, std):
+        locationList = []
+        for location in data:
+            locationList.append((location-mean)/std)
+        return locationList
+
     def move(self):
         input_direction = np.array([0, 0, 0])
 
@@ -124,7 +130,7 @@ class ModalOperator(bpy.types.Operator):
 
         # 포즈 특징 채우기
         poseStructs = self.motionMatcher.getCurrentPose()
-        featureStructs = self.motionMatcher.getCurrentFeature()
+        
         poseDBVelocity = poseStructs['mixamorig2:Hips']['velocity']
         speed = np.linalg.norm([poseDBVelocity[0]/100,poseDBVelocity[1]/100,poseDBVelocity[2]/100]) 
         if speed == 0: speed = ZERO_VELOCITY
@@ -140,10 +146,10 @@ class ModalOperator(bpy.types.Operator):
         rootVelocity = global2local(rootVelocity.tolist())
         print('루트 속도&글로벌 위치',rootVelocity, globalLocation )
 
-        RfootLocation = featureStructs['footLocation']['right']
+        RfootLocation = poseStructs['mixamorig2:RightFoot']['tailLocation']
         RfootVelocity= poseStructs['mixamorig2:RightFoot']['velocity']
 
-        LfootLocation = featureStructs['footLocation']['left']
+        LfootLocation = poseStructs['mixamorig2:LeftFoot']['tailLocation']
         LfootVelocity= poseStructs['mixamorig2:LeftFoot']['velocity']
 
 
@@ -172,8 +178,11 @@ class ModalOperator(bpy.types.Operator):
 
         #return rootVelocity + LfootVelocity + RfootVelocity + LfootLocation + RfootLocation + trajectoryLocation + trajectoryDirection
 
-        #return LfootLocation + RfootLocation + trajectoryLocation
+        # return self.calculateStandard(LfootLocation, features[-1]['mean']['Lfoot']['tailLocation'], features[-1]['std']['Lfoot']['tailLocation']) +self.calculateStandard(RfootLocation, features[-1]['mean']['Lfoot']['tailLocation'], features[-1]['std']['Lfoot']['tailLocation']) + self.calculateStandard(trajectoryLocation, features[-1]['mean']['hips']['location'], features[-1]['std']['hips']['location'])
+        # return self.calculateStandard(trajectoryLocation, features[-1]['mean']['hips']['location'], features[-1]['std']['hips']['location'])
         return trajectoryLocation
+
+   
 
     def calculateFutureTrajectory(self,timeDelta):
         omega = 2.0/self.smoothTime